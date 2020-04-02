4563a01fd0e331e7207b721b820701dc0d17dab3
AmateurECE <edtwardy@mtu.edu>
2020-04-02 10:49:40 -0400

    Create Test Fixture
    Using GTest.

---
+++ test/TCP/TCPIntegrationTest.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            TCPIntegrationTest.cpp
+//
+// AUTHOR:          Ethan D. Twardy <edtwardy@mtu.edu>
+//
+// DESCRIPTION:     Test file.
+//
+// CREATED:         04/02/2020
+//
+// LAST EDITED:     04/02/2020
+////
+
+#include "gtest/gtest.h"
+
+#include "TCPIntegrationTest.h"
+
+TEST_F(TCPIntegrationTest, CompileTest)
+{
+  ASSERT_EQ(2 + 2, 4);
+}
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ test/TCP/TCPIntegrationTest.h
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            TCPIntegrationTest.h
+//
+// AUTHOR:          Ethan D. Twardy <edtwardy@mtu.edu>
+//
+// DESCRIPTION:     Integration test for the TCP system.
+//
+// CREATED:         04/02/2020
+//
+// LAST EDITED:     04/02/2020
+////
+
+#ifndef __ET_TCPINTEGRATIONTEST__
+#define __ET_TCPINTEGRATIONTEST__
+
+#include "gtest/gtest.h"
+
+class TCPIntegrationTest : public ::testing::Test
+{};
+
+#endif // __ET_TCPINTEGRATIONTEST__
+
+///////////////////////////////////////////////////////////////////////////////
